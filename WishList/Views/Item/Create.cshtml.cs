3d6e617d37499981227fd2011fcf29a7a13dc779
taclearning22 <taclearning22@gmail.com>
2022-04-29 17:55:55 +0530

    Tasks for Module 5

---
+++ WishList/Views/Item/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WishList.Views.Item
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
