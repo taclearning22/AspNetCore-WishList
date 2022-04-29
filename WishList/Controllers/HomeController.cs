01dbfe8434d56fabf62563ecd924b340a1ef5f4d
taclearning22 <taclearning22@gmail.com>
2022-04-29 16:27:23 +0530

    Tasks for Module 3

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(string viewName)
+        {
+            return View("Index");
+        }
+        public IActionResult Error(string viewName)
+        {
+            return View("Error");
+        }
+    }
+}
---
+++ WishList/Views/Shared/Error.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WishList.Views.Shared
+{
+    public class ErrorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
